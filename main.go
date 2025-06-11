dcd37b129c672954821f1f3b3d86ac99aa755a13
Constantine <tmail99@gmail.com>
2025-06-11 10:02:32 +0300

    :

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 // Пользовательский тип данных person .
 // Эта структура удовлетворяет (satisfies) интерфейсу
-// Translator , т.к. имеет метод rudimentaryTranslator()
+// type translator interface {
+//	rudimentaryTranslator() string
+// }
+// т.к. имеет метод
+// func (p person) rudimentaryTranslator() string{}
 // не принимающий параметров и возвращающий строку.
 type person struct {
 	name     string
@@ -22,7 +26,7 @@ func main() {
 	fmt.Scan(&user.language)
 
 	// Встраивание (embedding) интерфейса (!?)
-	// Функция welcome принимает translator интерфейс в качестве аргумента,
+	// func welcome(out translator){} принимает translator интерфейс в качестве аргумента,
 	// а это означает, что ее можно использовать с любым типом, который удовлетворяет translator интерфейсу.
 	// В данном случае мы передаем user переменную (тип person структура),
 	// которая удовлетворяет translator интерфейсу, поскольку у нее есть rudimentaryTranslator() метод.
