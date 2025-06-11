ddef96c24805e76b4597224907282348c10d5e0e
Constantine <tmail99@gmail.com>
2025-06-11 10:45:41 +0300

    1. Тест без мока готов!

---
+++ person_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func Test_person_rudimentaryTranslator(t *testing.T) {
+	tests := []struct {
+		name string
+		p    person
+		want string
+	}{
+		{
+			name: "eng",
+			p: person{
+				name:     "John",
+				language: "eng",
+			},
+			want: "Glad to see you John!",
+		},
+		{
+			name: "esp",
+			p: person{
+				name:     "John",
+				language: "esp",
+			},
+			want: "Me alegro de verte, John!",
+		},
+		{
+			name: "other",
+			p: person{
+				name:     "John",
+				language: "jp",
+			},
+			want: "Sorry, I don't speak jp.",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.p.rudimentaryTranslator(); got != tt.want {
+				t.Errorf("Got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
